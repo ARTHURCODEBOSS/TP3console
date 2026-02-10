3036e4f95b7ca15ee3ebfd9497295cd8cefdd99f
NielsGrrr <niels.stiefvater@etu.univ-smb.fr>
2026-02-10 14:28:28 +0100

    save avant gemini

--- TP3console/Program.cs
+++ TP3console/Program.cs
@@ -1,4 +1,5 @@
-﻿using TP3console.Models.EntityFramework;
+﻿using Microsoft.EntityFrameworkCore;
+using TP3console.Models.EntityFramework;
 
 namespace TP3console
 {
@@ -6,7 +7,18 @@ namespace TP3console
     {
         static void Main(string[] args)
         {
-          
+            using (var ctx = new FilmsDbContext())
+            {
+
+                Film titanic = ctx.Films.AsNoTracking().First(f => f.Nom.Contains("Titanic"));
+
+                titanic.Description = "Un bateau échoué. Date : " + DateTime.Now.ToString();
+
+                int nbchanges = ctx.SaveChanges();
+
+                Console.WriteLine("Nombre de changements : " + nbchanges);
+
+            }
         }
     }
 }
